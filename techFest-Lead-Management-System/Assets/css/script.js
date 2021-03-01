752bdf2caeb2631c46c6a569f059148c1588b121
Roehit2001 <roehitr1@gmail.com>
2021-03-01 12:33:34 +0530

    display properties updated

--- techFest-Lead-Management-System/Assets/css/script.js
+++ techFest-Lead-Management-System/Assets/css/script.js
@@ -9,3 +9,47 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+// <-----------------------------VISUAL SCRIPT--------------------------->
+function vis(){
+	document.getElementById('Profile').style.display ="contents";
+document.getElementById('Analytics').style.display ="none";
+document.getElementById('Add_lead').style.display ="none";
+document.getElementById('lead_submitted').style.display ="none";
+document.getElementById('lead_assigned').style.display ="none";
+}
+function vis1(){
+	document.getElementById('Profile').style.display ="contents";
+document.getElementById('Analytics').style.display ="none";
+document.getElementById('Add_lead').style.display ="none";
+document.getElementById('lead_submitted').style.display ="none";
+document.getElementById('lead_assigned').style.display ="none";
+}
+function vis2(){
+	document.getElementById('Analytics').style.display ="contents";
+document.getElementById('Profile').style.display ="none";
+document.getElementById('Add_lead').style.display ="none";
+document.getElementById('lead_submitted').style.display ="none";
+document.getElementById('lead_assigned').style.display ="none";
+}
+function vis3(){
+	document.getElementById('Add_lead').style.display ="contents";
+document.getElementById('Profile').style.display ="none";
+document.getElementById('Analytics').style.display ="none";
+document.getElementById('lead_submitted').style.display ="none";
+document.getElementById('lead_assigned').style.display ="none";
+}
+function vis4(){
+	document.getElementById('lead_submitted').style.display ="contents";
+document.getElementById('Profile').style.display ="none";
+document.getElementById('Analytics').style.display ="none";
+document.getElementById('Add_lead').style.display ="none";
+document.getElementById('lead_assigned').style.display ="none";
+}
+function vis5(){
+	document.getElementById('lead_assigned').style.display ="contents";
+document.getElementById('Profile').style.display ="none";
+document.getElementById('Analytics').style.display ="none";
+document.getElementById('Add_lead').style.display ="none";
+document.getElementById('lead_submitted').style.display ="none";
+}
+// <-----------------------------VISUAL SCRIPT END ---------------------->
