d1ed0b543e7272c9f7adfe307775ad74bd5db49a
akaur1399 <sonuamandeep99@gmail.com>
2021-03-03 13:28:39 +0530

    lead submitted done only js left

---
+++ techFest-Lead-Management-System/Assets/JS/script.js
@@ -0,0 +1,72 @@
+function visFunction() {
+    document.getElementById("APPimg").style.display = "block";
+    document.getElementById("EXAMimg").style.display = "none";
+    document.getElementById("PROJimg").style.display = "none";
+    document.getElementById("INTimg").style.display = "none";
+   /*document.getElementById("RESimg").style.display = "none";*/
+   
+    document.getElementById("APPbtn").style.backgroundColor = "#F04705";
+    document.getElementById("EXAMbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("PROJbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("INTbtn").style.backgroundColor = "#6c63ff";
+    /*document.getElementById("RESbtn").style.backgroundColor = "#323335";*/
+}
+
+function visFunction1() {
+    document.getElementById("APPimg").style.display = "none";
+    document.getElementById("EXAMimg").style.display = "block";
+    document.getElementById("PROJimg").style.display = "none";
+    document.getElementById("INTimg").style.display = "none";
+    /*document.getElementById("RESimg").style.display = "none";*/
+
+    document.getElementById("APPbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("EXAMbtn").style.backgroundColor = "#F04705";
+    document.getElementById("PROJbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("INTbtn").style.backgroundColor = "#6c63ff";
+   /* document.getElementById("RESbtn").style.backgroundColor = "#323335";*/
+}
+
+function visFunction2() {
+    document.getElementById("APPimg").style.display = "none";
+    document.getElementById("EXAMimg").style.display = "none";
+    document.getElementById("PROJimg").style.display = "block";
+    document.getElementById("INTimg").style.display = "none";
+  /*  document.getElementById("RESimg").style.display = "none";*/
+
+    document.getElementById("APPbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("EXAMbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("PROJbtn").style.backgroundColor = "#F04705";
+    document.getElementById("INTbtn").style.backgroundColor = "#6c63ff";
+    /*document.getElementById("RESbtn").style.backgroundColor = "#323335";*/
+}
+
+function visFunction3() {
+    document.getElementById("APPimg").style.display = "none";
+    document.getElementById("EXAMimg").style.display = "none";
+    document.getElementById("PROJimg").style.display = "none";
+    document.getElementById("INTimg").style.display = "block";
+   /* document.getElementById("RESimg").style.display = "none";*/
+
+    document.getElementById("APPbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("EXAMbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("PROJbtn").style.backgroundColor = "#F04705";
+    document.getElementById("INTbtn").style.backgroundColor = "#6c63ff";
+    /*document.getElementById("RESbtn").style.backgroundColor = "#323335";*/
+}
+
+
+ 
+
+/*function visFunction4() {
+    document.getElementById("APPimg").style.display = "none";
+    document.getElementById("EXAMimg").style.display = "none";
+    document.getElementById("PROJimg").style.display = "none";
+    document.getElementById("INTimg").style.display = "none";
+    /*document.getElementById("RESimg").style.display = "block";
+
+    document.getElementById("APPbtn").style.backgroundColor = "#323335";
+    document.getElementById("EXAMbtn").style.backgroundColor = "#323335";
+    document.getElementById("PROJbtn").style.backgroundColor = "#323335";
+    document.getElementById("INTbtn").style.backgroundColor = "#323335";
+   /* document.getElementById("RESbtn").style.backgroundColor = "#0077ff";*/
+}
