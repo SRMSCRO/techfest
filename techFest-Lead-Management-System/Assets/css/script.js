4fda7a7d7ee22f8fdaa519ad87c4c555101b6a74
Roehit2001 <roehitr1@gmail.com>
2021-03-03 14:04:31 +0530

    css and js update

--- techFest-Lead-Management-System/Assets/css/script.js
+++ techFest-Lead-Management-System/Assets/css/script.js
@@ -37,6 +37,7 @@ document.getElementById('Profile').style.display ="none";
 document.getElementById('Analytics').style.display ="none";
 document.getElementById('lead_submitted').style.display ="none";
 document.getElementById('lead_assigned').style.display ="none";
+visFunction();
 }
 function vis4(){
 	document.getElementById('lead_submitted').style.display ="contents";
@@ -53,3 +54,60 @@ document.getElementById('Add_lead').style.display ="none";
 document.getElementById('lead_submitted').style.display ="none";
 }
 // <-----------------------------VISUAL SCRIPT END ---------------------->
+// <-----------------------------LEAD SUBMITTED ------------------------->
+function visFunction() {
+    document.getElementById("openimg").style.display = "block";
+    document.getElementById("validatedimg").style.display = "none";
+    document.getElementById("rejectedimg").style.display = "none";
+    document.getElementById("closedimg").style.display = "none";
+   
+   
+    document.getElementById("openbtn").style.backgroundColor = "#F04705";
+    document.getElementById("validatedbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("rejectedbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("closedbtn").style.backgroundColor = "#6c63ff";
+
+}
+
+function visFunction1() {
+    document.getElementById("openimg").style.display = "none";
+    document.getElementById("validatedimg").style.display = "block";
+    document.getElementById("rejectedimg").style.display = "none";
+    document.getElementById("closedimg").style.display = "none";
+
+
+    document.getElementById("openbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("validatedbtn").style.backgroundColor = "#F04705";
+    document.getElementById("rejectedbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("closedbtn").style.backgroundColor = "#6c63ff";
+
+}
+
+function visFunction2() {
+    document.getElementById("openimg").style.display = "none";
+    document.getElementById("validatedimg").style.display = "none";
+    document.getElementById("rejectedimg").style.display = "block";
+    document.getElementById("closedimg").style.display = "none";
+
+
+    document.getElementById("openbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("validatedbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("rejectedbtn").style.backgroundColor = "#F04705";
+    document.getElementById("closedbtn").style.backgroundColor = "#6c63ff";
+
+}
+
+function visFunction3() {
+    document.getElementById("openimg").style.display = "none";
+    document.getElementById("validatedimg").style.display = "none";
+    document.getElementById("rejectedimg").style.display = "none";
+    document.getElementById("closedimg").style.display = "block";
+
+
+    document.getElementById("openbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("validatedbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("rejectedbtn").style.backgroundColor = "#6c63ff";
+    document.getElementById("closedbtn").style.backgroundColor = "#F04705";
+
+}
+//<----------------------------LEAD SUBMITTED ENDS---------------------->
