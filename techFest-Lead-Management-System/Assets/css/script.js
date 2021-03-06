ba4a470727fa118c8de0bc2bc1bc9a36d72a68f1
Roehit2001 <roehitr1@gmail.com>
2021-03-06 11:14:03 +0530

    Added a sample modal at lead_submitted @akaur1399

--- techFest-Lead-Management-System/Assets/css/script.js
+++ techFest-Lead-Management-System/Assets/css/script.js
@@ -2,13 +2,13 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
 // <-----------------------------VISUAL SCRIPT--------------------------->
 function vis(){
 	document.getElementById('Profile').style.display ="contents";
@@ -226,3 +226,28 @@ function visFunction11() {
 
 }
 //<-----------------------------Filter segment admin page script --------------------------->
+//<-----------------------------Modal script------------------------------------------------>
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
