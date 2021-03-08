bcd9091b7e7375cc259b945e5dc9a3525ea0b2fc
Roehit2001 <roehitr1@gmail.com>
2021-03-08 12:43:31 +0530

    redirect and revert

--- Assets/css/myChart.js
+++ Assets/css/myChart.js
@@ -1,86 +1,86 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
     
-    type: 'bar',
+//     type: 'bar',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April','May', 'June'],
-        datasets: [{
-            label: 'Open Leads',
-            backgroundColor: '#505ae250',
-            borderColor: '#5059E2',
-            hoverBackgroundColor: '#505ae2ab',
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April','May', 'June'],
+//         datasets: [{
+//             label: 'Open Leads',
+//             backgroundColor: '#505ae250',
+//             borderColor: '#5059E2',
+//             hoverBackgroundColor: '#505ae2ab',
             
-            data: [3, 10, 20, 5,15,35],
+//             data: [3, 10, 20, 5,15,35],
            
-            borderWidth:1,
-        }]
-    },
+//             borderWidth:1,
+//         }]
+//     },
 
-    options: {
-        layout: {
-            padding: {
-                left: 20,
-                right: 20,
-                top: 20,
-                bottom: 20,
-            }
-        },
-        animation: {
-            duration: 2000,
-        },
-        legend: {
-            display: true,
-            labels: {
-                fontColor: '#5059E2'
-            }
-        },
+//     options: {
+//         layout: {
+//             padding: {
+//                 left: 20,
+//                 right: 20,
+//                 top: 20,
+//                 bottom: 20,
+//             }
+//         },
+//         animation: {
+//             duration: 2000,
+//         },
+//         legend: {
+//             display: true,
+//             labels: {
+//                 fontColor: '#5059E2'
+//             }
+//         },
         
-    }
-});
+//     }
+// });
 
-var ctx = document.getElementById('myChart1').getContext('2d');
-var chart = new Chart(ctx, {
+// var ctx = document.getElementById('myChart1').getContext('2d');
+// var chart = new Chart(ctx, {
     
-    type: 'bar',
+//     type: 'bar',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April','May', 'June'],
-        datasets: [{
-            label: 'Closed Leads',
-            backgroundColor: '#505ae250',
-            borderColor: '#5059E2',
-            hoverBackgroundColor: '#505ae2ab',
-            data: [3, 10, 20, 5,15,35],
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April','May', 'June'],
+//         datasets: [{
+//             label: 'Closed Leads',
+//             backgroundColor: '#505ae250',
+//             borderColor: '#5059E2',
+//             hoverBackgroundColor: '#505ae2ab',
+//             data: [3, 10, 20, 5,15,35],
 
            
-            borderWidth:1,
-        }]
-    },
+//             borderWidth:1,
+//         }]
+//     },
 
-    options: {
-        layout: {
-            padding: {
-                left: 20,
-                right: 20,
-                top: 20,
-                bottom: 20,
-            }
-        },
-        animation: {
-            duration: 2000,
-        },
-        legend: {
-            display: true,
-            labels: {
-                fontColor: '#5059E2'
-            }
-        },
+//     options: {
+//         layout: {
+//             padding: {
+//                 left: 20,
+//                 right: 20,
+//                 top: 20,
+//                 bottom: 20,
+//             }
+//         },
+//         animation: {
+//             duration: 2000,
+//         },
+//         legend: {
+//             display: true,
+//             labels: {
+//                 fontColor: '#5059E2'
+//             }
+//         },
        
-    }
-});
+//     }
+// });
 
 var ctx = document.getElementById('pieChart').getContext('2d');
 var myDoughnutChart = new Chart(ctx, {
